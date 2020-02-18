95e0f04ab3780be667d7fb9f3ba451881dfda84f
Matteo Amico <matteo@matteos-mbp.home>
2020-02-18 21:57:46 +0000

    restapi in GOLang

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+// Books Struct (Model)
+type Book struct {
+	ID string  `json:"id"`
+	Isbn string  `json:"isbn"`
+	Title string  `json:"title"`
+	Author *Author  `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+//Init books var as a slice Book struct
+var books []Book
+
+//Get all books
+func getBooks(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+
+func createBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+
+func updateBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+
+func deleteBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+
+func main()  {
+	//init Router
+	r := mux.NewRouter()
+
+	//Mock data - @todo  implement DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Bad Ass", Author: &Author{Firstname: "Jhon", Lastname: "Snow"}})
+	books = append(books, Book{ID: "2", Isbn: "448744", Title: "Capitan America", Author: &Author{Firstname: "Ron", Lastname: "Dallas"}})
+	books = append(books, Book{ID: "3", Isbn: "448745", Title: "Go land", Author: &Author{Firstname: "Rob", Lastname: "Pike"}})
+
+	//Route Handlers / Endpoints
+	r.HandleFunc("/api/books",getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}",getBook).Methods("GET")
+	r.HandleFunc("/api/books",createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}",updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}",deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
